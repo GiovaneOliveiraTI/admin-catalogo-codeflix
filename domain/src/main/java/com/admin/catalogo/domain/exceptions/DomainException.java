b1164aba9849ec855a13915b661574345f1a466d
giovaneoliveirati <giovaneolioveira_ti@hotmail.com>
2024-02-21 22:31:33 -0300

    Criando classes exceptions

--- domain/src/main/java/com/admin/catalogo/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/admin/catalogo/domain/exceptions/DomainException.java
@@ -1,2 +1,25 @@
-package com.admin.catalogo.domain.exceptions;public class DomainException {
+package com.admin.catalogo.domain.exceptions;
+
+import com.admin.catalogo.domain.validation.Error;
+import lombok.Getter;
+
+import java.util.List;
+@Getter
+public class DomainException extends NoStackTraceException {
+
+    private final List<Error> erros;
+    private DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
+        this.erros = anErrors;
+    }
+    public static DomainException with(final Error anErrors) {
+        return new DomainException("", List.of(anErrors));
+    }
+
+    public static DomainException with(final List<Error> anErrors) {
+        return new DomainException("", anErrors);
+    }
+
+
+
 }
