bff6defc18bea2b0ed3f3ca4a24ba86c96525e9b
giovaneoliveirati <giovaneolioveira_ti@hotmail.com>
2024-04-18 21:21:16 -0300

    Criando testes integrados

--- infrastructure/src/main/java/com/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -5,6 +5,7 @@ import com.admin.catalogo.domain.category.CategoryGateway;
 import com.admin.catalogo.domain.category.CategoryID;
 import com.admin.catalogo.domain.category.CategorySearchQuery;
 import com.admin.catalogo.domain.pagination.Pagination;
+import com.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,8 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Category create(Category aCategory) {
-        return null;
+        return this.repository.save(CategoryJpaEntity.from(aCategory))
+                .toAggregate();
     }
 
     @Override
---
+++ infrastructure/src/test/java/com/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.admin.catalogo.infrastructure.category.persistence;
+
+import com.admin.catalogo.infrastructure.MySQLGatewayTest;
+
+@MySQLGatewayTest
+public class CategoryRepositoryTest {
+}
