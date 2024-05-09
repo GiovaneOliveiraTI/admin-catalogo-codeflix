7165fc74e61273d46b687bb4a57e3497211ab44c
giovaneoliveirati <giovaneolioveira_ti@hotmail.com>
2024-05-08 22:26:23 -0300

    criacao de utils e finalizando testes unitarios

--- infrastructure/src/main/java/com/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -7,12 +7,19 @@ import com.admin.catalogo.domain.category.CategorySearchQuery;
 import com.admin.catalogo.domain.pagination.Pagination;
 import com.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+import static com.admin.catalogo.infrastructure.utils.SpecificationUtils.like;
+
 @Service
 public class CategoryMySQLGateway implements CategoryGateway {
+
 
     private final CategoryRepository repository;
 
@@ -21,21 +28,22 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public Category create(Category aCategory) {
+    public Category create(final Category aCategory) {
         return save(aCategory);
     }
 
     @Override
-    public void deleteById(CategoryID anId) {
-        String anIdValue = anId.getValue();
-        if(this.repository.existsById(anIdValue)){
+    public void deleteById(final CategoryID anId) {
+        final String anIdValue = anId.getValue();
+        if (this.repository.existsById(anIdValue)) {
             this.repository.deleteById(anIdValue);
         }
     }
 
     @Override
-    public Optional<Category> findById(CategoryID anId) {
-        return Optional.empty();
+    public Optional<Category> findById(final CategoryID anId) {
+        return this.repository.findById(anId.getValue())
+                .map(CategoryJpaEntity::toAggregate);
     }
 
     @Override
@@ -44,8 +52,33 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
-        return null;
+    public Pagination<Category> findAll(final CategorySearchQuery aQuery) {
+        // Paginação
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        // Busca dinamica pelo criterio terms (name ou description)
+        final var specifications = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map(str -> {
+                    final Specification<CategoryJpaEntity> nameLike = like("name", str);
+                    final Specification<CategoryJpaEntity> descriptionLike = like("description", str);
+                    return nameLike.or(descriptionLike);
+                })
+                .orElse(null);
+
+        final var pageResult =
+                this.repository.findAll(Specification.where(specifications), page);
+
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(CategoryJpaEntity::toAggregate).toList()
+        );
     }
 
     private Category save(final Category aCategory) {
