1bdeeb336373c587810f01df717f870de8251d35
giovaneoliveirati <giovaneolioveira_ti@hotmail.com>
2024-05-21 05:44:32 -0300

    configs de test integrations e implementacao tests integration

--- infrastructure/src/main/java/com/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/admin/catalogo/infrastructure/Main.java
@@ -1,9 +1,18 @@
 package com.admin.catalogo.infrastructure;
 
+import com.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
+import com.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
+import com.admin.catalogo.application.category.retrieve.list.ListCategoriesUseCase;
+import com.admin.catalogo.application.category.update.UpdateCategoryUseCase;
 import com.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -11,4 +20,17 @@ public class Main {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
+
+    ApplicationRunner runner (
+            @Autowired CreateCategoryUseCase createCategoryUseCase,
+            @Autowired UpdateCategoryUseCase updateCategoryUseCase,
+            @Autowired DeleteCategoryUseCase deleteCategoryUseCase,
+            @Autowired ListCategoriesUseCase listCategoriesUseCase,
+            @Autowired GetCategoryByIdUseCase getCategoryByIdUseCase
+    ) {
+        return args -> {
+
+        };
+    }
+
 }
